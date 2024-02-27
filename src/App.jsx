ee53112ea53a9cc2d1a015ecb7f882fc1c93a987
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-27 13:07:30 -0300

    modularizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,12 @@
 import { useState } from 'react';
+import confetti from "canvas-confetti";
 import './App.css'
 
-const TURNOS = {
-  X: "x",
-  O:"o"
-}
+import { Square } from './assets/Components/square.jsx';
+
+import { WINNER_COMBOS, TURNOS } from './Constants/constants.jsx';
 
 const tablero = Array(9).fill(null);
-
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
-
-
-const Square = ({ children, isSelected, updateBoard, index, }) => {
-
-  const className = `square ${isSelected ? 'is-selected' : ''} `
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div
-      className={className}
-      onClick={handleClick}
-    >
-      {children}
-    </div>
-  )
-}
 
 function App() {
 
@@ -83,6 +54,7 @@ function App() {
     const newWinner = checkWninner(newBoard);
     if (newWinner) {
       setWinner(newWinner);
+      confetti();
     } else if (checkEndGame(newBoard)) {
       //Empate
       setWinner(false);
